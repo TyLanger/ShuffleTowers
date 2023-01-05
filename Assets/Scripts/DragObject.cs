c6c94881127fa8ca79b8d1ee25c63ee1e1c61dea
TyLanger <ty.langer16@gmail.com>
2023-01-04 21:17:09 -0600

    TileTypes
    Grass, Forest, Mountain, Water
    enum
    
    don't really do anything, just change the colours.

--- Assets/Scripts/DragObject.cs
+++ Assets/Scripts/DragObject.cs
@@ -7,45 +7,25 @@ public class DragObject : MonoBehaviour
 {
 
     private Vector3 mOffset;
-    float mzCoord;
 
     Coords myCoords;
 
     private Vector3 GetMouseWorldPos()
     {
-        /*Vector3 mousePoint = Input.mousePosition;
-        mousePoint.z = mzCoord;
-
-        //Vector3 mouseWorld = Camera.main.ScreenToWorldPoint(mousePoint);
-        //return new Vector3(mouseWorld.x, 1.0f, mouseWorld.z);
-        return Camera.main.ScreenToWorldPoint(mousePoint);*/
-
-
         return MousePosition.Instance.GridPosition;
     }
 
     private void OnMouseDown()
     {
-        mzCoord = Camera.main.WorldToScreenPoint(transform.position).z;
-        // mzCoord = 0.0f;
-
         mOffset = transform.position - GetMouseWorldPos();
 
         myCoords = GridBuilder.Instance.PositionToCoords(transform.position);
-
-        gameObject.GetComponent<MeshRenderer>().material.color = Color.red;
     }
 
     private void OnMouseDrag()
     {
-        // this isn't quite right
-        // but close enough
-        // I don't want the tiles to go up and down based on perspective, but oh well
-        //Vector3 mouse = GetMouseWorldPos();
-        //transform.position = new Vector3(mouse.x + mOffset.x, transform.position.y, mouse.z + mOffset.z);
         transform.position = GetMouseWorldPos() + mOffset + Vector3.up;
         Swap();
-        
     }
 
     private void OnMouseUp()
@@ -56,8 +36,6 @@ public class DragObject : MonoBehaviour
     void Swap()
     {
         Coords c = GridBuilder.Instance.PositionToCoords(transform.position);
-        //Debug.Log($"Position: {transform.position}, Coords: {c.x}, {c.y}");
-        //Debug.Log($"In bounds? {GridBuilder.Instance.IsInBounds(c)}");
 
         if (!myCoords.Equals(c))
         {
--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -8,12 +8,13 @@ public class Tile : MonoBehaviour
     Vector3 desiredPostion;
 
     [SerializeField] float moveSpeed = 10;
+    [SerializeField] TileType tileType;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetRandomTileType();
     }
 
     // Update is called once per frame
@@ -26,4 +27,41 @@ public class Tile : MonoBehaviour
     {
         desiredPostion = pos;
     }
+
+    public void SetTileType(TileType newType)
+    {
+        tileType = newType;
+
+        var mat = GetComponent<MeshRenderer>().material;
+        switch(tileType)
+        {
+            case TileType.Grass:
+                mat.color = Color.yellow;
+                break;
+
+            case TileType.Forest:
+                mat.color = Color.green;
+                break;
+
+            case TileType.Mountain:
+                mat.color = Color.red;
+                break;
+
+            case TileType.Water:
+                mat.color = Color.blue;
+                break;
+        }
+    }
+
+    public void SetRandomTileType()
+    {
+        var r = Random.Range(0, 4);
+
+        SetTileType((TileType)r);
+    }
+}
+
+public enum TileType
+{
+    Grass, Forest, Mountain, Water
 }
